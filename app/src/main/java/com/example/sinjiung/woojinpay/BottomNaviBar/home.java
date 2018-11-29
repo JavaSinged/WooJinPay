d92dac5a450d4a7b650e9fcee4b0213163b93fa4
javasinged <shinjiwoong1656@gmail.com>
2018-11-29 13:16:05 +0900

    그리드 뷰, 클릭 효과

--- app/src/main/java/com/example/sinjiung/woojinpay/BottomNaviBar/home.java
+++ app/src/main/java/com/example/sinjiung/woojinpay/BottomNaviBar/home.java
@@ -2,48 +2,59 @@ package com.example.sinjiung.woojinpay.BottomNaviBar;
 
 import android.content.Context;
 import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.Toast;
 
 import com.example.sinjiung.woojinpay.Product.Item;
+import com.example.sinjiung.woojinpay.Product.ProductImage;
 import com.example.sinjiung.woojinpay.Product.RecyclerAdapter;
 import com.example.sinjiung.woojinpay.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 
 public class home extends Fragment {
 
     RecyclerView ProductCase;
+    RecyclerAdapter recyclerAdapter = new RecyclerAdapter();
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //화면 inflate
         ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_home, container, false);
         ProductCase = (RecyclerView)rootView.findViewById(R.id.productCase);
+
+
         GridLayoutManager layoutManager = new GridLayoutManager(getContext().getApplicationContext(), 2);
         ProductCase.setHasFixedSize(true);
         ProductCase.setLayoutManager(layoutManager);
+        ProductCase.setAdapter(recyclerAdapter);
 
-        List<Item> items = new ArrayList<>();
-        Item[] item = new Item[3];
-        item[0] = new Item(R.drawable.ic_home_black_24dp, "#1");
-        item[1] = new Item(R.drawable.ic_launcher_background, "#2");
-        item[2] = new Item(R.drawable.ic_power_settings_new_black_24dp, "#3");
-        for(int i=0;i<3;i++){
-            items.add(item[i]);
-        }
-        ProductCase.setAdapter(new RecyclerAdapter(getContext().getApplicationContext(), items,R.layout.fragment_home));
+        recyclerAdapter.setItems(new ProductImage().getItems());
+
         return rootView;
     }
 }
